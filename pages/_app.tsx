aee74323919cb63c013c41ca51c30fb35d780fa7
Lucas Matheus Santos Cardoso <lucasmatheussc97@gmail.com>
2020-07-02 20:25:50 -0300

    absolute path of css imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
-import 'primereact/resources/themes/nova-light/theme.css'
-import 'primereact/resources/primereact.min.css'
-import 'primeicons/primeicons.css'
-import 'react-markdown-editor-lite/lib/index.css';
+import '../node_modules/primereact/resources/themes/nova-light/theme.css'
+import '../node_modules/primereact/resources/primereact.min.css'
+import '../node_modules/primeicons/primeicons.css'
+import '../node_modules/react-markdown-editor-lite/lib/index.css';
 import {AuthProvider} from '../contexts/authenticantion.context'
 
 
