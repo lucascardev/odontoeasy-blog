008941f33804a9f1a807d1a468433194d6df9ff2
Lucas Matheus Santos Cardoso <lucasmatheussc97@gmail.com>
2020-07-02 20:38:14 -0300

    changes for styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,5 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
-import '../node_modules/primereact/resources/themes/nova-light/theme.css'
-import '../node_modules/primereact/resources/primereact.min.css'
-import '../node_modules/primeicons/primeicons.css'
-import '../node_modules/react-markdown-editor-lite/lib/index.css';
 import {AuthProvider} from '../contexts/authenticantion.context'
 
 
